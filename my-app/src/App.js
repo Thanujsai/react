d35f929036972f11a659aecf168bfbd91eed139b
Thanujsai <thanuj.sai14301@gmail.com>
2023-08-15 20:18:26 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import { Navbar } from './components/Navbar.js';
+import { About } from './components/About';
+import { Information } from './components/Information';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route> 
+        <Route exact path='/about' element={<About />}></Route>
+        <Route exact path='/information' element={<Information />}></Route>
+      </Routes>
+  );
+}
+
+export default App;
